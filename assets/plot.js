27d9a655e6b6794ebb556c300a4a9947b4ccc7c7
dylanlholt <dylan.lee.holt@gmail.com>
2016-12-12 22:49:27 -0500

    Added Fixed Nodes

--- assets/plot.js
+++ assets/plot.js
@@ -1,5 +1,5 @@
-var width = 960,
-    height = 500;
+var width = 1000,
+    height = 700;
 
 var force = d3.layout.force()
     .size([width, height])
@@ -29,12 +29,20 @@ d3.json("graph.json", function(error, graph) {
     .enter().append("line")
       .attr("class", "link");
 
-  node = node.data(graph.nodes)
+  node1 = node.data(graph.nodes.filter(function(d,i){return i != 23 && i != 11 && i != 59}))
     .enter().append("circle")
       .attr("class", "node")
       .attr("r", 10)
+      .on("click", function(d,i){console.log(i)})
       .on("dblclick", dblclick)
       .call(drag);
+
+  node2 = node.data(graph.nodes.filter(function(d,i){return i == 23 || i == 11 || i == 59}))
+    .enter().append("polygon")
+      .attr("class", "node")
+      .attr("fill","blue")
+      //.attr("points", function(d){return d.x+','+d.y+' '(d.x+5)+','+(d.y-10)+' '+(d.x+10)+','+d.y})
+      .on("click", function(d,i){console.log(i)})
 });
 
 function tick() {
@@ -43,8 +51,11 @@ function tick() {
       .attr("x2", function(d) { return d.target.x; })
       .attr("y2", function(d) { return d.target.y; });
 
-  node.attr("cx", function(d) { return d.x; })
+  node1.attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; });
+
+  node2.attr("points", function(d){return (d.x-12)+','+(d.y+12)+' '+(d.x)+','+(d.y-12)+' '+(d.x+12)+','+(d.y+12);})
+       .classed("fixed", function(d){d.fixed = true})
 }
 
 function dblclick(d) {
