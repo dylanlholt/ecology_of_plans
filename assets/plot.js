3327634b7a7183fe36e7f09773faf296c15fadfe
dylanlholt <dylan.lee.holt@gmail.com>
2016-12-14 16:12:30 -0500

    Created App and ForceGraph Objects

--- assets/plot.js
+++ assets/plot.js
@@ -1,3 +1,44 @@
+//Application Object
+
+var app;
+
+d3.json("graph.json", function(error, json) {
+  if (error) return console.warn(error);
+  app.init(json)
+});
+
+app = {
+  data: {
+    nodes: [],
+    links: [],
+  },
+
+  components: [],
+
+  init: function(data){
+
+    console.log(data)
+
+    app.data.nodes = data.nodes.map(function(d){
+      return {x:d.x,y:d.y,id:1}
+    });
+    app.data.links = data.links
+
+    //console.log(app.data.nodes[70].id)
+
+    app.components = [new forceGraph('#forceGraph')];
+
+  }
+}
+
+forceGraph = function(el){
+  this.setup();
+}
+
+forceGraph.prototype = {
+
+setup: function(){
+
 var width = 1000,
     height = 700;
 
@@ -17,8 +58,8 @@ var svg = d3.select("body").append("svg")
 var link = svg.selectAll(".link"),
     node = svg.selectAll(".node");
 
-d3.json("graph.json", function(error, graph) {
-  if (error) throw error;
+console.log(app.data)
+var graph = app.data;
 
   force
       .nodes(graph.nodes)
@@ -29,23 +70,27 @@ d3.json("graph.json", function(error, graph) {
     .enter().append("line")
       .attr("class", "link");
 
-  node1 = node.data(graph.nodes.filter(function(d,i){return i != 23 && i != 11 && i != 59}))
+  node1 = node.data(graph.nodes.filter(function(d,i){return i % 13 != 0}))
     .enter().append("circle")
-      .attr("class", "node")
+      .attr("class", "node pri")
       .attr("r", 10)
       .on("click", function(d,i){console.log(i)})
       .on("dblclick", dblclick)
       .call(drag);
 
-  node2 = node.data(graph.nodes.filter(function(d,i){return i == 23 || i == 11 || i == 59}))
+  node2 = node.data(graph.nodes.filter(function(d,i){return i % 13 == 0}))
     .enter().append("polygon")
-      .attr("class", "node")
-      .attr("fill","blue")
+      .attr("class", "node pub")
       //.attr("points", function(d){return d.x+','+d.y+' '(d.x+5)+','+(d.y-10)+' '+(d.x+10)+','+d.y})
       .on("click", function(d,i){console.log(i)})
-});
+
+//console.log(graph.nodes.length);
 
 function tick() {
+
+  console.log('Inside Tick Function')
+  //console.log(force.alpha())
+
   link.attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
@@ -56,6 +101,11 @@ function tick() {
 
   node2.attr("points", function(d){return (d.x-12)+','+(d.y+12)+' '+(d.x)+','+(d.y-12)+' '+(d.x+12)+','+(d.y+12);})
        .classed("fixed", function(d){d.fixed = true})
+
+  if (force.alpha() < 0.05){
+      force.stop();
+      console.log('DONE!!!')
+    }
 }
 
 function dblclick(d) {
@@ -64,4 +114,6 @@ function dblclick(d) {
 
 function dragstart(d) {
   d3.select(this).classed("fixed", d.fixed = true);
+}
+}
 }
